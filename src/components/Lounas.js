718448ab3e2bcd9d1ee022a6021d8a3aef9e8796
sharifmajumdar <sharifmajumdar@gmail.com>
2026-03-19 17:44:09 +0200

    lunch link

--- src/components/Lounas.js
+++ src/components/Lounas.js
@@ -1,12 +1,35 @@
 import React from "react";
 
 const Lounas = () => {
-  return(
-  <span>
-    Katso päivän lista:
-    https://www.lounaat.info/lounas/ravintola-taj-mahal/joensuu
- </span>
-  )
+  return (
+    <div
+      name="lounas"
+      className="w-full min-h-screen bg-white dark:bg-gradient-to-b from-gray-800 to-black text-gray-500 py-10"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl text-gray-500 font-bold inline border-b-4 border-grey-900">
+            Lounas
+          </p>
+        </div>
+        <p className="text-xl mt-5 text-justify">
+          Lounas aika: 10.00 - 16.00 (Ma - Pe)
+        </p>
+        <p className="text-xl mt-5 text-justify">
+          Hinta: 13,50€ (ePassi, edenred, smartum, käteinen ja korti maksu) ja Lapsi: 1€/ikävuosi
+        </p>
+        <p className="text-xl mt-5 text-justify">
+          Alustava menu: <br/>
+        </p>
+        <p>
+               Katso päivän lista: <a href="https://www.lounaat.info/lounas/ravintola-taj-mahal/joensuu" target="_blank" rel="noopener noreferrer">
+            lounaat.info
+            </a>
+        </p>
+
+      </div>
+    </div>
+  );
 
 /*   useEffect(() => {
     // Load the script dynamically
